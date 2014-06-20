550d3445dcd125a0c20007d98c45cf913e16dd43
Dan Borza <dan.borza@intel.com>
2014-06-20 12:03:33 +0300

    Building book json and displaying image

--- js/controllers.js
+++ js/controllers.js
@@ -5,12 +5,21 @@ var appControllers = angular.module('appControllers', []);
 appControllers.controller('TestController', ['$scope', '$sce',
   function ($scope, $sce) {
     $scope.testValue = 'caca-maca-value';
-    $scope.imageUrl = 'img/phones/dell-venue.1.jpg';
+    $scope.imageUrl = 'img/phones/1.jpg';
 
-    $scope.trustAsResourceUrl = function(url) {
+    $scope.trustAsResourceUrl = function (url) {
       console.log("running trustAsResourceUrl: " + url);
       console.log("sce service: ", $sce);
       console.log($sce.trustAsResourceUrl("img/phones/dell-venue.0.jpg"));
       return $sce.trustAsResourceUrl(url);
+    }
+
+    $scope.books = []
+
+    for (var i = 1; i <= 14; i++) {
+      $scope.books.push({
+        title: "title" + i,
+        path: "img/phones/" + i + ".jpg"
+      });
     }
   }]);
