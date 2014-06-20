ed9a46e12172f76e79236c6ac737827a436f7e1b
Dan Borza <dan.borza@intel.com>
2014-06-20 11:35:30 +0300

    Displaying image

--- js/app.js
+++ js/app.js
@@ -3,3 +3,16 @@
 var appModule = angular.module('appModule', [
   'appControllers'
 ]);
+
+appModule.config([
+  '$compileProvider',
+  function ($compileProvider) {
+    var currentImgSrcSanitizationWhitelist = $compileProvider.imgSrcSanitizationWhitelist();
+    var newImgSrcSanitizationWhiteList = currentImgSrcSanitizationWhitelist.toString().slice(0, -1)
+      + '|chrome-extension:'
+      + currentImgSrcSanitizationWhitelist.toString().slice(-1);
+
+    console.log("Changing imgSrcSanitizationWhiteList from " + currentImgSrcSanitizationWhitelist + " to " + newImgSrcSanitizationWhiteList);
+    $compileProvider.imgSrcSanitizationWhitelist(newImgSrcSanitizationWhiteList);
+  }
+]);
--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,15 @@
 
 var appControllers = angular.module('appControllers', []);
 
-appControllers.controller('TestController', ['$scope',
-  function ($scope) {
-    $scope.testValue='caca-maca-value'
+appControllers.controller('TestController', ['$scope', '$sce',
+  function ($scope, $sce) {
+    $scope.testValue = 'caca-maca-value';
+    $scope.imageUrl = 'img/phones/dell-venue.1.jpg';
+
+    $scope.trustAsResourceUrl = function(url) {
+      console.log("running trustAsResourceUrl: " + url);
+      console.log("sce service: ", $sce);
+      console.log($sce.trustAsResourceUrl("img/phones/dell-venue.0.jpg"));
+      return $sce.trustAsResourceUrl(url);
+    }
   }]);
